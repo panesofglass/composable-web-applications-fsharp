0a0152c2ef584061feac7c0aa46e961f0d1b61e7
Ryan Riley <ryan.riley@panesofglass.org>
2012-09-13 16:59:14 -0500

    Add sample WebSharper composition

---
+++ WebFormsSharper/Website/SignupSequence.fs
@@ -0,0 +1,10 @@
+﻿namespace Website
+
+open IntelliFactory.WebSharper
+open IntelliFactory.WebSharper.Html
+
+type SignupSequence() =
+    inherit Web.Control()
+
+    [<JavaScript>]
+    override this.Body = ContactForms.SignupSequence () :> _
